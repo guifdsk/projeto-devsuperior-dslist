613117082d6aaa1e66767751656bd510a124e496
guifdsk <gui.fdsk@gmail.com>
2023-05-13 19:41:45 -0300

    feat: implementando endpoint lists - #aula2

---
+++ dslist/src/main/java/com/devsuperior/dslist/controlers/GameListController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslist.controlers;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.services.GameListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService service;
+
+    @GetMapping
+    public List<GameListDTO> findAll(){
+        return service.findAll();
+    }
+
+}
