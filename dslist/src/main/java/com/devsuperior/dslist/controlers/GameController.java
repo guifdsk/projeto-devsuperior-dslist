b53bab3563fdcecad6a086bbad6ea2ae090a4d32
guifdsk <gui.fdsk@gmail.com>
2023-05-13 19:18:08 -0300

    feat: adicionando endpoit de busca por id - #aula2

--- dslist/src/main/java/com/devsuperior/dslist/controlers/GameController.java
+++ dslist/src/main/java/com/devsuperior/dslist/controlers/GameController.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dslist.controlers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,12 @@ public class GameController {
     private GameService service;
 
     @GetMapping
-    public List<GameMinDTO> findAl(){
+    public List<GameMinDTO> findAll(){
          return service.findAll();
+    }
+
+    @GetMapping(value = "/{id_game}")
+    public GameDTO findById(@PathVariable(value = "id_game") Long id){
+        return service.findById(id);
     }
 }
